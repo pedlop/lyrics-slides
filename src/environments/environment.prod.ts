2de5f62a2b32358c5e204906d4adbf95fdcc163b
Pedro <pedrolopes09.pv@gmail.com>
2019-08-23 10:59:30 -0300

    Adding config keys to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCyXvt-N-NFFySy_gvdsSmM8oYa4Kn0yFc",
+    authDomain: "lyrics-slides.firebaseapp.com",
+    databaseURL: "https://lyrics-slides.firebaseio.com",
+    projectId: "lyrics-slides",
+    storageBucket: "",
+    messagingSenderId: "469452091224",
+    appId: "1:469452091224:web:824563eb4a122506"
+  }
 };
